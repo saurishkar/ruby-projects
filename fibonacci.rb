dc7bc59538f8345668e2e99f3953ba421149fc02
vinsol <basicallysaurish@gmail.com>
2018-11-18 16:31:33 +0530

    completed fibonacci number generation

---
+++ fibonacci.rb
@@ -0,0 +1,24 @@
+public def generate_fibonacci(count)
+	i = 1
+	a = 0
+	b = 1
+	c = 0
+	while i <= count
+		c = a + b
+		yield c
+		a = b
+		b = c
+		i = i + 1
+	end 
+end
+
+puts "Enter the count of numbers to display"
+
+count = gets.chomp.to_i
+
+if count > 0
+	generate_fibonacci(count) { |num| printf("%i ", num) }
+	puts ""
+else
+	puts "Enter a valid Input"
+end
