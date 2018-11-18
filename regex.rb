f74a3f33039b842b8d10bb60ee7f764e73eae0dc
vinsol <basicallysaurish@gmail.com>
2018-11-18 15:44:28 +0530

    completed vowel replace program

---
+++ regex.rb
@@ -0,0 +1,16 @@
+def replace_vowel(str)
+	str.gsub!(/a+|e+|i+|o+|u+/, "*")
+	puts str
+end
+
+puts "Enter a string"
+
+strInput = gets.chomp
+
+if strInput.length == 0
+	puts "Please provide an input"
+else
+	replace_vowel(strInput)
+end
+
+
