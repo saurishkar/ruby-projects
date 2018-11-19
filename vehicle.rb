b3551c3125246a18c2f7a3716dc53d61e23b3a99
vinsol <basicallysaurish@gmail.com>
2018-11-20 00:16:48 +0530

    minor fix

--- vehicle.rb
+++ vehicle.rb
@@ -5,22 +5,38 @@ class Vehicle
   end
 
   def to_s
-    "Bike Name: #{@name} \n Bike Price: #{@price}" 
+    "\nBike Name: #{@name} \nBike Price: #{@price}\n" 
   end
 
 end
 
 class Bike < Vehicle
-  def initialize(dealer, percent_price_increase)
+  def initialize(vehicleName, price, dealer, percent_price_increase)
+    super(vehicleName, price)
     @dealer = dealer
     @percent_price_increase = percent_price_increase
   end
 
-  def display_contents
-    puts "Bike Dealer: #{@dealer}"
+  def to_s
+    super + "Bike Dealer: #{@dealer}"
   end
 
-  def to_s
-    "Bike Dealer: #{@dealer} Price Increase : #{@percent_price_increase}"
+  def price_increase
+      @price = (@price * (1 + @percent_price_increase / 100)).round(2)
   end
 end
+
+puts "Enter input"
+strInput = gets.chomp
+
+if strInput.nil?
+  puts "Enter a valid Input"
+else
+  inputArr = strInput.split(/\s+/)
+  bikeObj = Bike.new(inputArr[0], inputArr[1].to_f, inputArr[2], inputArr[3].to_f)
+  puts bikeObj
+  puts " ", "After #{inputArr[3].to_f} percent price hike:"
+  bikeObj.price_increase
+  puts bikeObj
+end
+
