d170ed7b990f4a8ecf110c4bd59bec04cf58311e
vinsol <basicallysaurish@gmail.com>
2018-11-20 12:28:45 +0530

    Refactored code

--- regex.rb
+++ regex.rb
@@ -1,6 +1,5 @@
 def replace_vowel(str)
-  str.gsub!(/a+|e+|i+|o+|u+/, "*")
-  puts str
+  str.gsub(/[aeiou]/, "*")
 end
 
 puts "Enter a string"
@@ -10,7 +9,7 @@ strInput = gets.chomp
 if strInput.length == 0
   puts "Please provide an input"
 else
-  replace_vowel(strInput)
+  puts replace_vowel(strInput)
 end
 
 
