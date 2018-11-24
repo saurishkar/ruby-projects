93e2de9d852d4e7d7c13d9eb157d3891dc8adfae
Saurish Kar <basicallysaurish@gmail.com>
2018-11-24 06:26:20 +0530

    minor refactoring

--- account.rb
+++ account.rb
@@ -1,49 +1,43 @@
+# ACCOUNT CLASS
 class Account
   @@account_number = 0
-  def initialize(name, balance)
-    @name = name
+  def initialize(holder_name, balance)
+    @name = holder_name
     @account_no = @@account_number + 1
     @balance = balance
 
-    @@account_number = @@account_number + 1
+    @@account_number += 1
   end
 
   def deposit(amount)
-    @balance = @balance + amount.to_i
+    @balance += amount.to_i
   end
 
   def withdraw(amount)
-    @balance = @balance - amount.to_i
+    @balance -= amount.to_i
   end
 
-  def view_account()
-    puts "Account Number: #{@account_no}"
-    puts "Account Holder Name: #{@name}"
-    puts "Account Balance: #{@balance}"
+  def view_account
+    puts "Account number: #{@account_no}"
+    puts "Account holder name: #{@name}"
+    puts "Account balance: #{@balance}"
   end
-
 end
 
-
-puts "Enter input"
-str = gets.chomp
-
-if str.nil?
-  puts "Enter a valid Input"
+if ARGV.empty?
+  puts 'Please provide an input'
 else
-  accountObjArr = []
-  arr = str.split(/\s+/)
+  acc_obj_arr = []
 
-  arr.each do |st|
+  ARGV.each do |st|
     params = st.split(/:/)
-    if params[0] == "transfer"
-      accountObjArr[0].withdraw(params[1])
-      accountObjArr[1].deposit(params[1])
-    else 
-      accountObjArr.push(Account.new(params[0].gsub!(/\"+/, ""), params[1].to_i))
+    if params[0] == 'transfer'
+      acc_obj_arr[0].withdraw(params[1])
+      acc_obj_arr[1].deposit(params[1])
+    else
+      acc_obj_arr.push(Account.new(params[0].gsub(/\"+/, ''), params[1].to_i))
     end
   end
-
-  puts accountObjArr[0].view_account
-  puts accountObjArr[1].view_account
+  puts acc_obj_arr[0].view_account
+  puts acc_obj_arr[1].view_account
 end
