0a57edddd0e02a72f6ee2157aeed90490fc0061c
Saurish Kar <basicallysaurish@gmail.com>
2018-12-05 10:22:28 +0530

    minor change

--- vehicle.rb
+++ vehicle.rb
@@ -2,11 +2,11 @@
 class Vehicle
   def initialize(vehicle_name, price)
     @name = vehicle_name
-    @price = price.to_f
+    @price = price
   end
 
   def to_s
-    "\nBike Name: #{@name} \nBike Price: #{@price}\n"
+    "Bike Name: #{@name}\nBike Price: #{@price}\n"
   end
 end
 
@@ -15,15 +15,15 @@ class Bike < Vehicle
   def initialize(vehicle_name, price, dealer, percent_price_increase)
     super(vehicle_name, price)
     @dealer = dealer
-    @percent_price_increase = percent_price_increase.to_f
+    @percent_price_increase = percent_price_increase
   end
 
   def to_s
-    super + "Bike Dealer: #{@dealer}"
+    super + "Bike Dealer: #{@dealer}\n"
   end
 
   def price_increase
-    @price = (@price * (1 + @percent_price_increase / 100)).round(2)
+    @price = (@price.to_f * (1 + @percent_price_increase.to_f / 100)).round(2)
   end
 end
 
@@ -32,7 +32,7 @@ if ARGV.empty?
 else
   bike_obj = Bike.new(ARGV[0], ARGV[1], ARGV[2], ARGV[3])
   puts bike_obj
-  puts ' ', "After #{ARGV[3].to_f} percent price hike:"
+  print "\nAfter #{ARGV[3].to_f} percent hike in price:\n"
   bike_obj.price_increase
   puts bike_obj
 end
